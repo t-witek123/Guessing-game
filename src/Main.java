a7412335a79b1c4b2ee2fed3bfebdc78e139a968
t-witek123 <t_witek@protonmail.com>
2020-05-04 11:26:44 +0200

    Utworzenie nagłówka

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("This is going to be a guessing game that you can play if you are mature person");
+        Scanner scanner =new Scanner(System.in);
+        System.out.println("How old are You? ");
+        int age =scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What's Your name? ");
+        String name =scanner.nextLine();
+
+        System.out.println("Your age: "+ age+ "Your name is: "+ name);
 
     }
 }
