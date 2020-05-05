837cd455bec789f6d5924df0487cae5d9ee0d5a9
t-witek123 <t_witek@protonmail.com>
2020-05-05 14:13:34 +0200

    Utworzenie pętli i warunków- rozpoczęcie programu, jeśli wiek jest ok// problem- zatrzymuje program nawet jeśli odpowiedź jest nieprawidlowa

--- src/Main.java
+++ src/Main.java
@@ -9,13 +9,39 @@ public class Main {
         scanner.nextLine();
         System.out.println("What's Your name? ");
         String name =scanner.nextLine();
-        System.out.println("Your age: "+ age+ "Your name is: "+ name);
+        System.out.println("Your name is: " + name + "; Your are: " + age + " y.o");
 
         if (age>=18) {
             System.out.println("Your age is about right, you can play this sick game.");
-        }else {
-            System.out.println("Sorry man, You need to grow up to play it :) ");
+            System.out.println("");
+            System.out.println("Enter your guess: ");
+            int hidden = 25;
+            int guess = scanner.nextInt();
+            do {
+
+
+                if (guess==hidden){
+                    System.out.println("Congratulation, your guess is correct");
+                    break;
+                }else{
+                    if (guess>hidden) {
+                        System.out.println("Try lower number");
+                    }
+                    if (guess<hidden) {
+                        System.out.println("Try higher number");
+                    }
+                }
+
+            }while (guess==hidden);
+
+            }else {
+                System.out.println("Sorry man, You need to grow up to play it :) ");
         }
+
+
+
+
+
 
     }
 }
